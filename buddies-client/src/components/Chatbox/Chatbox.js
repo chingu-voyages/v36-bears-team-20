7e3d1d597957bf5944e91779c89d0fbbfd608801
Tingfeng <wu.tingfeng@u.nus.edu>
2022-02-18 19:14:53 +0800

    Loading indicator for chat messages

--- buddies-client/src/components/Chatbox/Chatbox.js
+++ buddies-client/src/components/Chatbox/Chatbox.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 
-import { Box } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 import axios from "axios";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
@@ -191,34 +191,48 @@ export default function Chatbox({ socket }) {
   }
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <SideDrawer {...{ chatsAsGuest, chatsAsHost, setCurrentChatId }} />
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateAreas: `"header"
+    <Box
+      sx={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+      }}
+      alignItems="center"
+      justifyContent="center"
+    >
+      {loaded ? (
+        <>
+          <SideDrawer {...{ chatsAsGuest, chatsAsHost, setCurrentChatId }} />
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateAreas: `"header"
                         "chat"
                         "textinput"`,
-          gridTemplateColumns: "auto",
-          gridTemplateRows: "64px minmax(0, 1fr) 56px",
-          bgcolor: (theme) => theme.palette.background.paper,
-          padding: 0,
-          minHeight: "100vh",
-          flexGrow: 1,
-        }}
-      >
-        {currentChat !== undefined && (
-          <>
-            <TopBar
-              {...{
-                counterPartyName: counterPartyName,
-              }}
-            />
-            <ChatMessages messages={messages} userid={user._id} />
-            <TextInput {...{ currentChatId, socket }} />
-          </>
-        )}
-      </Box>
+              gridTemplateColumns: "auto",
+              gridTemplateRows: "64px minmax(0, 1fr) 56px",
+              bgcolor: (theme) => theme.palette.background.paper,
+              padding: 0,
+              minHeight: "100vh",
+              flexGrow: 1,
+            }}
+          >
+            {currentChat !== undefined && (
+              <>
+                <TopBar
+                  {...{
+                    counterPartyName: counterPartyName,
+                  }}
+                />
+                <ChatMessages messages={messages} userid={user._id} />
+                <TextInput {...{ currentChatId, socket }} />
+              </>
+            )}
+          </Box>
+        </>
+      ) : (
+        <CircularProgress size="4rem" />
+      )}
     </Box>
   );
 }
